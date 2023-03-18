c4db513ad575049fdae5b5d273ab325f9ce415ff
髙師蒼平 <so.takashi1118@gmail.com>
2023-03-18 15:22:54 +0900

    first-commit

---
+++ MyJanken/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MyJanken
+//
+//  Created by 高師蒼平 on 2023/03/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("gu")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyJanken/MyJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJankenApp.swift
+//  MyJanken
+//
+//  Created by 高師蒼平 on 2023/03/18.
+//
+
+import SwiftUI
+
+@main
+struct MyJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
